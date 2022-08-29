8cc240ce27923356e8f9bd80870cccfe10280fcf
DVelnc4 <davidvalencia4@gmail.com>
2022-08-29 13:49:53 -0500

    instancias y clase tasklist

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,22 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.time.LocalDate;
+
+//@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		//SpringApplication.run(DemoApplication.class, args);
+
+		Task task1 = new Task("Tarea 1: Aprender Java", false, LocalDate.of(2022,12,31));
+
+		System.out.println(task1.getDescription());
+
+		task1.setDescription("Tarea 1: Aprender Java y Desarrollo Web");
+
+		System.out.println(task1.getDescription());
 	}
 
 }
