dfdad1866fa10dbf9a3816993159588efa8137be
DVelnc4 <varrieta.a@gmail.com>
2022-08-29 13:11:16 -0500

    constructor and setters getters

--- src/main/java/com/example/demo/Task.java
+++ src/main/java/com/example/demo/Task.java
@@ -1,4 +1,39 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+
 public class Task {
+    private String description;
+    private boolean done;
+    private LocalDate dueDate;
+
+    public Task(String description, boolean done, LocalDate dueDate) {
+        this.description = description;
+        this.done = done;
+        this.dueDate = dueDate;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return this.done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public LocalDate getDueDate() {
+        return this.dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
 }
